ff75d5ff062d604b31d2203db3d6c9b3f34e62cf
kumar4321-create <74403511+kumar4321-create@users.noreply.github.com>
2020-11-27 15:41:24 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,9 +1,13 @@
 # When authenticating using the Azure CLI or a Service Principal:
+provider "azurerm" {
+  version         = "0.11.13"
+  features {}
+}
 terraform {
   backend "azurerm" {
     storage_account_name = "tfstacc0101"
     container_name       = "terraform"
     key                  = "dev.terraform.tfstate"
-    version         = "0.11.13"
-  }
+    }
 }
+
